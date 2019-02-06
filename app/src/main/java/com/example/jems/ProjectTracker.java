34e9f9de35e99f0fc938314bbb5707fab3074728
Jaime <Jaimeperales123@gmail.com>
2019-02-06 10:37:43 -0600

    test Branch for main screen

---
+++ app/src/main/java/com/example/jems/ProjectTracker.java
@@ -0,0 +1,13 @@
+package com.example.jems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_tracker);
+    }
+}
