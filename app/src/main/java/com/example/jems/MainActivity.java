9aeeb360a400b2d6786dfa0ecda3135cde7b81fd
Jaime <Jaimeperales123@gmail.com>
2019-02-06 11:10:57 -0600

    test Branch for main screen

--- app/src/main/java/com/example/jems/MainActivity.java
+++ app/src/main/java/com/example/jems/MainActivity.java
@@ -1,15 +1,9 @@
 package com.example.jems;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,34 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     /** Called when the user taps the quickEstimate button */
     public void startQuickEstimate(View view)
@@ -61,5 +28,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
-
 }
