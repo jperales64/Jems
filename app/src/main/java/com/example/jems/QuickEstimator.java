9b7e5286bf4af0d02f9c4b4de9165ec4e696fd5b
sfowler12 <i.scottfowler@gmail.com>
2019-02-26 17:40:12 -0600

    Added elements to Estimator

--- app/src/main/java/com/example/jems/QuickEstimator.java
+++ app/src/main/java/com/example/jems/QuickEstimator.java
@@ -2,12 +2,34 @@ package com.example.jems;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuickEstimator extends AppCompatActivity {
+
+    private ListView materialsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_estimator);
+
+        materialsListView = (ListView) findViewById(R.id.materialsListView);
+
+        //Temporary List containing sample items.
+        //TODO: create database interfacer to populate lists
+        List<String> materialsArrayList = new ArrayList<String>();
+        materialsArrayList.add("1\" x 5-1/2\" x 6' Pressure Treated Pine Dog-Ear Fence Picket\t380\t684.00");
+        materialsArrayList.add("2\" x 4\" x 8' Pressure Treated Dimensional Lumber\t73\t$240.90");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                materialsArrayList);
+
+        materialsListView.setAdapter(arrayAdapter);
     }
 }
