c399c5bb6047e885144ff29c4e819753d0326d88
Scott Fowler <i.scottfowler@gmail.com>
2019-02-27 20:02:53 -0600

    Reformatted material types for better enumeration organization

--- app/src/main/java/com/example/jems/Material.java
+++ app/src/main/java/com/example/jems/Material.java
@@ -1,11 +1,24 @@
 package com.example.jems;
 
+import com.example.jems.materials.MaterialType;
+
 class Material {
-    //Change to money api
-    private Float cost;
+
+    String name;
+    Float cost;     //Change to money api
+    MaterialType materialType;
+
+    public Material(String name, MaterialType materialType, Float cost){
+        this.name = name;
+        this.materialType = materialType;
+        this.cost = cost;
+    }
 
     public Material(String name, Float cost){
+        this.name = name;
+        this.materialType = MaterialType.Misc.EMPTY;
         this.cost = cost;
+        System.out.println(MaterialType.Siding.BRICK);
     }
 
     public Float getCost() {
@@ -14,5 +27,10 @@ class Material {
 
     public void setCost(Float cost) {
         this.cost = cost;
+    }
+
+    @Override
+    public String toString(){
+        return name + "\t" + materialType.label() + "\t" + cost;
     }
 }
