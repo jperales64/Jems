e57658afbd4c0934613469a45cbe9bb7a430a7c8
sfowler12 <i.scottfowler@gmail.com>
2019-03-01 15:24:22 -0600

    Completed GUI for Estimator screen

--- app/src/main/java/com/example/jems/QuickEstimator.java
+++ app/src/main/java/com/example/jems/QuickEstimator.java
@@ -21,24 +21,24 @@ public class QuickEstimator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_estimator);
 
-        materialsListView = (ListView) findViewById(R.id.materialsListView);
+        //materialsListView = (ListView) findViewById(R.id.materialsListView);
 
         //Temporary List containing sample items.
         //TODO: create database interfacer to populate lists
 
-        Material fencePicket = new Material("1\" x 5-1/2\" x 6' Pressure Treated Pine Dog-Ear " +
-                "Fence Picket", MaterialType.Fencing.PICKET, new BigDecimal("1.8"));
-
-
-        List<String> materialsArrayList = new ArrayList<String>();
-        materialsArrayList.add("1\" x 5-1/2\" x 6' Pressure Treated Pine Dog-Ear Fence Picket\t380\t684.00");
-        materialsArrayList.add("2\" x 4\" x 8' Pressure Treated Dimensional Lumber\t73\t$240.90");
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                materialsArrayList);
-
-        materialsListView.setAdapter(arrayAdapter);
+//        Material fencePicket = new Material("1\" x 5-1/2\" x 6' Pressure Treated Pine Dog-Ear " +
+//                "Fence Picket", MaterialType.Fencing.PICKET, new BigDecimal("1.8"));
+//
+//
+//        List<String> materialsArrayList = new ArrayList<String>();
+//        materialsArrayList.add("1\" x 5-1/2\" x 6' Pressure Treated Pine Dog-Ear Fence Picket\t380\t684.00");
+//        materialsArrayList.add("2\" x 4\" x 8' Pressure Treated Dimensional Lumber\t73\t$240.90");
+//
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                materialsArrayList);
+//
+//        materialsListView.setAdapter(arrayAdapter);
     }
 }
